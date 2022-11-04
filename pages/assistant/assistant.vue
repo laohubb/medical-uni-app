5ce60138996947ac97d5d799f2cb20012f2bc16b
laohu <laohubbb@gmail.com>
2022-11-04 23:32:31 +0800

    增加轮播图

---
+++ pages/assistant/assistant.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		助手阿森阿是
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,15 @@
 <template>
 	<view>
-			home
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item>
+				<view class="swiper-item">
+				
+					<img src="../../static/swiper/2.jpeg" alt="" srcset="">
+
+				</view>
+			</swiper-item>
+
+		</swiper>
 	</view>
 </template>
 
@@ -8,12 +17,24 @@
 	export default {
 		data() {
 			return {
-				
+				swiperList: ['']
 			};
 		}
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 320rpx;
+		
+		.swiper-item {
+			height: 100%;
+		
+			img {
+				width: 100%;
+		
+			}
+		}
 
+	}
 </style>
